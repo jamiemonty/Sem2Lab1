7205d72d2f19f597debf7bd460e6208b1dc0b7fe
Jamie <G00419525@atu.ie>
2024-02-02 11:40:31 +0000

    Started new project

--- src/main/java/ie/atu/productv3/ProductDB.java
+++ src/main/java/ie/atu/productv3/ProductDB.java
@@ -2,6 +2,31 @@ package ie.atu.productv3;
 
 public class ProductDB {
 
+    ie.atu.productv2.Software mySoftware = null;
+    if (productCode.equalsIgnoreCase("studios")) {
+        mySoftware = new ie.atu.productv2.Software();
+        mySoftware.setCode(productCode);
+        mySoftware.setDescription("Visual Studios");
+        mySoftware.setPrice(57.50);
+        mySoftware.setVersion("Microsoft 1.1");
+    } else if (productCode.equalsIgnoreCase("eclipse")) {
+        mySoftware = new ie.atu.productv2.Software();
+        mySoftware.setCode(productCode);
+        mySoftware.setDescription("Build Java apps");
+        mySoftware.setPrice(57.50);
+        mySoftware.setVersion("Eclipse Neon");
+    } else if (productCode.equalsIgnoreCase("oracle")) {
+        mySoftware = new Software();
+        mySoftware.setCode(productCode);
+        mySoftware.setDescription("Latest MySQL");
+        mySoftware.setPrice(54.50);
+        mySoftware.setVersion("Oracle 3.0");
+    }
+
+
+
+
+        return mySoftware;
     //  public static Book or Software getProduct(String productCode) {
     // In a more realistic application, this code would get the data for the product from a file or database
     // For now, this code just uses if/else statements to return the correct product data
